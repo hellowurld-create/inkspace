863c00d752649a0e29744563067da4fa18712c6a
hellowurld-create <madebyaugus@gmail.com>
2024-03-04 04:00:30 +0100

    STRIPE HANDLING

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,9 +2,14 @@ import Link from "next/link"
 import MaxWidthWrapper from "./MaxWidthWrapper"
 import { ArrowRight, Pen, Pencil } from "lucide-react"
 import { buttonVariants } from "./ui/button"
-import {LoginLink, RegisterLink} from '@kinde-oss/kinde-auth-nextjs/server'
+import {LoginLink, RegisterLink, getKindeServerSession} from '@kinde-oss/kinde-auth-nextjs/server'
+import UserAccountNav from "./UserAccountNav"
 
 const NavBar = () => {
+
+    const {getUser} = getKindeServerSession()
+    const user = getUser()
+
     return (
         <nav className="w-full border-b border-gray-300 bg-white/75 backdrop-blur-lg sticky h-14 top-0 inset-x-0 z-30 transition-all">
             <MaxWidthWrapper>
@@ -19,6 +24,8 @@ const NavBar = () => {
                     {/* Add mobile navbar */}
 
                     <div className="hidden items-center space-x-4 sm:flex">
+                        {
+                            !user ?
                         <>
                             <Link
                                 href='/pricing'
@@ -44,8 +51,25 @@ const NavBar = () => {
                             >
                                 Get started <ArrowRight className="ml-1.5 h-4 w-4"/>
                             </RegisterLink>
-                        </>
-                    </div>
+                        </> :
+                                <>
+                                 <Link
+                                href='/dashboard'
+                                className={buttonVariants({
+                                    variant: 'ghost',
+                                    size: 'sm'
+                            })}
+                            >
+                                Dashboard
+                                    </Link>
+                                    <UserAccountNav name={
+                                        !user.given_name || !user.family_name ? 'Your Account' : `${user.given_name} ${user.given_name}`
+                                     }
+                                        email={user.email ?? ''}
+                                        imageUrl={user.picture ?? ''}
+                                    />
+                                </>
+                    }</div>
                 </div>
             </MaxWidthWrapper>
         </nav>
